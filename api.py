6e97b2d462d0b1cecb014cc80d63486899e5c47f
littlestone0806 <1039398195@qq.com>
2023-05-09 15:51:37 +0800

    Create api.py

---
+++ api.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, Request
+from transformers import AutoTokenizer, AutoModel
+import uvicorn, json, datetime
+import torch
+from sse_starlette.sse import EventSourceResponse
+
+DEVICE = "cuda"
+DEVICE_ID = "0"
+CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(CUDA_DEVICE):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+
+app = FastAPI()
+
+
+def predict_stream(tokenizer, prompt, history, max_length, top_p, temperature):
+    for response, history in model.stream_chat(tokenizer, prompt, history, max_length=max_length, top_p=top_p,
+                                               temperature=temperature):
+        now = datetime.datetime.now()
+        time = now.strftime("%Y-%m-%d %H:%M:%S")
+        yield json.dumps({
+            'response': response,
+            'history': history,
+            'status': 200,
+            'time': time
+        })
+    log = "[" + time + "] " + '", prompt:"' + prompt + '", response:"' + repr(response) + '"'
+    print(log)
+    return torch_gc()
+
+
+def predict(tokenizer, prompt, history, max_length, top_p, temperature):
+    response, history = model.chat(tokenizer,
+                                   prompt,
+                                   history=history,
+                                   max_length=max_length if max_length else 2048,
+                                   top_p=top_p if top_p else 0.7,
+                                   temperature=temperature if temperature else 0.95)
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    answer = {
+        "response": response,
+        "history": history,
+        "status": 200,
+        "time": time
+    }
+    log = "[" + time + "] " + '", prompt:"' + prompt + '", response:"' + repr(response) + '"'
+    print(log)
+    torch_gc()
+    return answer
+
+@app.post("/")
+async def create_item(request: Request):
+    global model, tokenizer
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+    history = json_post_list.get('history')
+    max_length = json_post_list.get('max_length')
+    top_p = json_post_list.get('top_p')
+    temperature = json_post_list.get('temperature')
+    stream = json_post_list.get('stream')
+    if stream:
+        res = predict_stream(tokenizer, prompt, history, max_length, top_p, temperature)
+        return EventSourceResponse(res)
+    else:
+        answer = predict(tokenizer, prompt, history, max_length, top_p, temperature)
+        return answer
+
+
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained("model", trust_remote_code=True)
+    model = AutoModel.from_pretrained("model", trust_remote_code=True).half().cuda()
+    model.eval()
+    uvicorn.run(app, host='192.168.3.86', port=7860, workers=1)
